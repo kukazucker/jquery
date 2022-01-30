52176ff772119259500dd98a764c412919c6e5b7
kukazucker <85445965+kukazucker@users.noreply.github.com>
2022-01-30 23:53:11 +0200

    Create main.js

---
+++ delivery_form/JS/main.js
@@ -0,0 +1,16 @@
+select();
+checkTypePack("#mainBlock__current", "#typePack0");
+
+counter('#text', "", "#weight");
+counter('#text1', 1, "#volume");
+
+boxName("#text_Person1", "#personFrst");
+boxName("#text_Person2", "#personScnd");
+
+check("#myCheckboxId1", "#checkbox__box_1", ".checkbox_box1");
+check("#myCheckboxId2", "#checkbox__box_2", ".checkbox_box2");
+check("#myCheckboxId3", "#checkbox__box_3", ".checkbox_box3");
+check("#myCheckboxId4", "#checkbox__box_4", ".checkbox_box4");
+
+
+formPrice(".boldWords", "#price", "#btn_configurate");
